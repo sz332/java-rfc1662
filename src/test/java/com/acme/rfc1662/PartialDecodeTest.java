a72eeb97c05de1db75e434499ece54a1a06f7ea0
Zsolt Sandor <sz332@users.noreply.github.com>
2018-12-21 11:27:15 +0100

    extended test case with partial to full message

--- src/test/java/com/acme/rfc1662/PartialDecodeTest.java
+++ src/test/java/com/acme/rfc1662/PartialDecodeTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static com.acme.rfc1662.IntArray.join;
 
 public class PartialDecodeTest {
-	
+
 	@Test
 	public void testPartialHeaderArrived() {
 		ParserResult result = new PPPParser().parse(join(new int[] { 0x7E }));
@@ -49,6 +49,53 @@ public class PartialDecodeTest {
 		Assert.assertNotNull(result);
 		Assert.assertEquals(0, result.getMessages().size());
 		Assert.assertEquals(6, result.getRemaining().length);
+	}
+
+	@Test
+	public void testPartialContentArrived() {
+		ParserResult result = new PPPParser().parse(join(new int[] { 0x7E, 0xFF, 0x7D, 0x23, 0xC0, 0x21, 0x01 }));
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals(0, result.getMessages().size());
+		Assert.assertEquals(7, result.getRemaining().length);
+	}
+
+	@Test
+	public void testMoreContentArrived() {
+		ParserResult result = new PPPParser().parse(join(new int[] { 0x7E, 0xFF, 0x7D, 0x23, 0xC0, 0x21, 0x01, 0x02 }));
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals(0, result.getMessages().size());
+		Assert.assertEquals(8, result.getRemaining().length);
+	}
+
+	@Test
+	public void testPartialCRCArrived() {
+		ParserResult result = new PPPParser().parse(join(new int[] { 0x7E, 0xFF, 0x7D, 0x23, 0xC0, 0x21, 0x01, 0x02, 0x9d }));
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals(0, result.getMessages().size());
+		Assert.assertEquals(9, result.getRemaining().length);
+	}
+
+	@Test
+	public void testCRCArrived() {
+		ParserResult result = new PPPParser().parse(join(new int[] { 0x7E, 0xFF, 0x7D, 0x23, 0xC0, 0x21, 0x01, 0x02, 0x9d, 0x4f }));
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals(0, result.getMessages().size());
+		Assert.assertEquals(10, result.getRemaining().length);
+	}
+
+	@Test
+	public void testFullMessageArrived() {
+		ParserResult result = new PPPParser().parse(join(new int[] { 0x7E, 0xFF, 0x7D, 0x23, 0xC0, 0x21, 0x01, 0x02, 0x9d, 0x4f, 0x7E }));
+
+		Assert.assertNotNull(result);
+		Assert.assertEquals(1, result.getMessages().size());
+		Assert.assertEquals(0, result.getRemaining().length);
+
+		Assert.assertArrayEquals(result.getMessages().get(0), new byte[] { 0x01, 0x02 });
 	}
 
 }
